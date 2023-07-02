258cc49be8706a1bae290c0d24dd9e4d8e214f31
mgporter <26415062+mgporter@users.noreply.github.com>
2023-07-02 00:27:10 -0400

    rebuild html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpg|jpeg|svg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    
-}
+  mode: 'production',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
